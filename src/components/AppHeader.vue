6ee161319f64a0774ae6663530a58060a5b46c8f
P4anix <pancok_1@yahoo.it>
2023-03-27 16:30:58 +0200

    bootstrap e header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,28 @@
 <script>
-    export default {
-        name: 'Header'
-    }
+export default {
+    name: 'Header'
+}
 </script>
 
 <template>
-    <header>
-        <h1>Yu-Gi-Oh Api</h1>
+    <header class="row p-2">
+        <div class="col text-center d-flex align-items-center justify-content-center">
+            <img src="http://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+                alt="logo">
+        </div>
+
+        <div class="col-11">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+
+
     </header>
 </template>
 
 
 
 <style lang="scss" scoped>
+img {
+    width: 50%;
+}
 </style>
