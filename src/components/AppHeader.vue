39ad05f2f6193069971d2544841a7ebb0ce75579
P4anix <pancok_1@yahoo.it>
2023-03-27 15:58:39 +0200

    AppMain, AppHeader e fonts

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+
+
+<style lang="scss" scoped>
+</style>
